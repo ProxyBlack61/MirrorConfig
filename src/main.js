80b3543bb2301f3032255f9b04faa8b166a6a3df
Kilian <kiliansc5@gmail.com>
2022-10-29 10:00:05 +0200

    Added VueGoogleMaps

--- src/main.js
+++ src/main.js
@@ -5,5 +5,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 import i18n from "./plugins/i18n";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import VueGoogleMaps from "vue-google-maps-community-fork";
 
-createApp(App).use(router).use(i18n).mount("#app");
+createApp(App)
+  .use(router)
+  .use(i18n)
+  .use(VueGoogleMaps, {
+    load: {
+      key: "AIzaSyBGBbrFcx0b-Nx59ieQIcOLGaU_PCLTBBo",
+      libraries: "places",
+    },
+  })
+  .mount("#app");
