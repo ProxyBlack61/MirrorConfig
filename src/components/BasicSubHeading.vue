368a06b66f184654ed78f6823681f93ee3404ec2
Kilian <kiliansc5@gmail.com>
2022-10-29 09:56:47 +0200

    Added BasicSubHeading Component

---
+++ src/components/BasicSubHeading.vue
@@ -0,0 +1,19 @@
+<template>
+  <hr style="width: 75%" class="text-center mx-auto" />
+  <p class="fw-bold mb-0 text-center">{{ $t(textString) }}</p>
+  <p class="text-center mx-auto">
+    {{ $t(textString + "Detail") }}
+  </p>
+  <hr style="width: 75%" class="text-center mx-auto" />
+</template>
+
+<script>
+export default {
+  name: "BasicSubHeading",
+  props: {
+    textString: String,
+  },
+};
+</script>
+
+<style scoped></style>
