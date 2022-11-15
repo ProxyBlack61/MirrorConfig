7e018094ee60e7fcb17b3ef984f862f67ac3f621
Kilian <kiliansc5@gmail.com>
2022-11-15 21:14:43 +0100

    Added position & active function to Weather.vue

--- src/views/Weather.vue
+++ src/views/Weather.vue
@@ -1,11 +1,144 @@
 <template>
-  <h1>Weather</h1>
   <!-- TODO: Implement Weather.vue -->
+  <div class="container-fluid">
+    <h1 class="display-4 text-center">{{ $t("weather.header") }}</h1>
+    <basic-sub-heading textString="weather.GeneralHeader" />
+
+    <div class="list-group mb-5 shadow">
+      <basic-toggle
+        textString="weather.Enabled"
+        v-model="moduleEnabled"
+        :true-value="false"
+        :false-value="true"
+      />
+      <basic-select
+        textString="Position"
+        v-model="weatherParent.module.position"
+        :options="positionOptions"
+        :disabled="weatherParent.module.disabled"
+      />
+    </div>
+
+    <div class="row" style="padding-left: 15%; padding-right: 15%">
+      <v-btn size="x-large" color="indigo" @click="saveWeatherSettings">
+        {{ $t("buttonSave") }}
+      </v-btn>
+    </div>
+
+    <v-snackbar v-model="snackbar" location="bottom right">
+      {{ $t("snackbarSaved") }}
+      <template v-slot:actions>
+        <v-btn color="pink" variant="text" @click="snackbar = false">
+          {{ $t("snackbarClose") }}
+        </v-btn>
+      </template>
+    </v-snackbar>
+    <hr />
+  </div>
 </template>
 
 <script>
+import BasicSubHeading from "../components/BasicSubHeading.vue";
+import BasicToggle from "../components/BasicToggle.vue";
+import BasicSelect from "../components/BasicSelect.vue";
+
 export default {
+  // eslint-disable-next-line vue/multi-word-component-names
   name: "Weather",
+  components: {
+    BasicSubHeading,
+    BasicToggle,
+    BasicSelect,
+  },
+  data() {
+    return {
+      snackbar: false,
+      weatherParent: {
+        module: {
+          disabled: false,
+          module: "weather",
+          position: "top_right",
+          config: {},
+        },
+      },
+      positionOptions: [
+        { id: 1, value: "top_bar", language: "PositionTopBar" },
+        { id: 2, value: "top_left", language: "PositionTopLeft" },
+        { id: 3, value: "top_center", language: "PositionTopCenter" },
+        { id: 4, value: "top_right", language: "PositionTopRight" },
+        { id: 5, value: "upper_third", language: "PositionUpperThird" },
+        {
+          id: 6,
+          value: "middle_center",
+          language: "PositionMiddleCenter",
+        },
+        { id: 7, value: "lower_third", language: "PositionLowerThird" },
+        { id: 8, value: "bottom_left", language: "PositionBottomLeft" },
+        {
+          id: 9,
+          value: "bottom_center",
+          language: "PositionBottomCenter",
+        },
+        {
+          id: 10,
+          value: "bottom_right",
+          language: "PositionBottomRight",
+        },
+        { id: 11, value: "bottom_bar", language: "PositionBottomBar" },
+        {
+          id: 12,
+          value: "fullscreen_above",
+          language: "PositionFullscreenAbove",
+        },
+        {
+          id: 13,
+          value: "fullscreen_below",
+          language: "PositionFullscreenBelow",
+        },
+      ],
+    };
+  },
+  created() {
+    fetch("http://127.0.0.1:3000/weatherSettings", {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (Object.keys(data.module).length !== 0) {
+          this.weatherParent.module = data.module;
+        }
+      });
+  },
+  methods: {
+    saveWeatherSettings() {
+      fetch("http://127.0.0.1:3000/weatherSettings", {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(this.weatherParent),
+      }).then((response) => {
+        if (response.ok) {
+          this.snackbar = true;
+        }
+      });
+    },
+  },
+  computed: {
+    moduleEnabled: {
+      get() {
+        return !this.weatherParent.module.disabled;
+      },
+      set(value) {
+        this.weatherParent.module.disabled = !value;
+      },
+    },
+  },
 };
 </script>
 
